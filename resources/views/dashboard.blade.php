924d7d1bfa3e3f0e693899eac0b8dbc3ec9bf3a4
BrayaCheat <prayacheat@gmail.com>
2025-03-07 18:32:28 +0700

    feat: impl breeze middleware

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                <h1 class="text-4xl font-bold text-white">Hello, It's Me Braya Smos.</h1>
                     {{ __("You're logged in!") }}
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/braya-smos', function (){
+    return view('braya');
+})->middleware(['auth', 'verified'])->name('braya');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
