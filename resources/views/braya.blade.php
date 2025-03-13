0f463a80e6d38759a8b5480953b6d4a9c6a3f3d3
BrayaCheat <prayacheat@gmail.com>
2025-03-13 18:14:31 +0700

    fix: using layout on page braya

--- resources/views/braya.blade.php
+++ resources/views/braya.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Braya Smos</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-</head>
+<x-app-layout>
+  <body class="flex items-center justify-center h-screen bg-gradient-to-r from-purple-500 to-indigo-600 text-white">
+    <h1 class="text-4xl font-bold">Hello, It's Me Braya Smos.</h1>
+  </body>
+</x-app-layout>
 
-<body class="flex items-center justify-center h-screen bg-gradient-to-r from-purple-500 to-indigo-600 text-white">
-  <h1 class="text-4xl font-bold">Hello, It's Me Braya Smos.</h1>
-</body>
 
-</html>
